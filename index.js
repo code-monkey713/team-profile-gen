464ea05d24d55d6f2259255955f3b2d516c8bc45
Tony Tran <codemonkey713@gmail.com>
2021-02-06 21:19:54 -0600

    setting up index as the start of npm start and required libraries

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const employee = require('./lib/Employee');
+const engineer = require('./lib/Engineer');
+const intern = require('./lib/Intern');
+const manager = require('./lib/Manager');
+
