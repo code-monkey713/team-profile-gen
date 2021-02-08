06996b39a87343a85c12aa5eddf7c81f2bce767e
Tony Tran <codemonkey713@gmail.com>
2021-02-07 23:24:02 -0600

    added Teamprofile class to start the team profile generator; linked it to index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const employee = require('./lib/Employee');
-const engineer = require('./lib/Engineer');
-const intern = require('./lib/Intern');
-const manager = require('./lib/Manager');
+// const inquirer = require('inquirer');
+// const fs = require('fs');
+// const employee = require('./lib/Employee');
+// const engineer = require('./lib/Engineer');
+// const intern = require('./lib/Intern');
+// const manager = require('./lib/Manager');
+// const questions = require('./lib/questions');
+const Teamprofile = require('./lib/Teamprofile')
 
+const teamprofile = new Teamprofile();
+
+teamprofile.askToStart();
