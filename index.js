ab25e3a81787bd225f82724631270fbd8e475aa4
Tony Tran <codemonkey713@gmail.com>
2021-02-09 00:41:31 -0600

    added questions for all classes so they won't repeat

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-// const inquirer = require('inquirer');
-// const fs = require('fs');
+const inquirer = require('inquirer');
+const fs = require('fs');
+// const open = require('open');
 // const employee = require('./lib/Employee');
 // const engineer = require('./lib/Engineer');
 // const intern = require('./lib/Intern');
 // const manager = require('./lib/Manager');
-// const questions = require('./lib/questions');
+const questions = require('./lib/questions');
 const Teamprofile = require('./lib/Teamprofile')
 
-const teamprofile = new Teamprofile();
+async function start() {
+  try {
+    const teamprofile = new Teamprofile();
+    teamprofile.askToStart();
 
-teamprofile.askToStart();
+    // const html = buildHTMLTemplate(userData);
+    // fs.writeFileSync('./index.html', html);
+    console.log('File was successfully written.');
+    // await open('./index.html');
+    console.log('Check your browser!');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
