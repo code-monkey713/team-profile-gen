d677474233c4ff4d32579503729edd891a6d034e
Tony Tran <codemonkey713@gmail.com>
2021-02-09 23:56:45 -0600

    added function for getManager, getEngineer, getIntern and to add new class to their respective array

--- index.js
+++ index.js
@@ -2,31 +2,60 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const questions = require('./lib/questions');
 // const open = require('open');
-// const employee = require('./lib/Employee');
-// const engineer = require('./lib/Engineer');
-// const intern = require('./lib/Intern');
-// const manager = require('./lib/Manager');
-// const questions = require('./lib/questions');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 // const Teamprofile = require('./lib/Teamprofile')
+
+let managerArray = [];
+let engineerArray = [];
+let internArray = [];
 
 async function start() {
   try {
     await inquirer.prompt(questions.welcome);
-    
-    const manager = await inquirer.prompt(questions.manager);
-    console.log(manager);
-
-    
-
-    const engineer = await inquirer.prompt(questions.engineer);
-//     const teamprofile = new Teamprofile();
-//     teamprofile.askToStart();
+    getManager();
+    getEngineer();
+    getIntern();
 
     // const html = buildHTMLTemplate(userData);
     // fs.writeFileSync('./index.html', html);
     // console.log('File was successfully written.');
     // await open('./index.html');
     // console.log('Check your browser!');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getManager() {
+  try {
+    const m = await inquirer.prompt(questions.manager);
+    const newManager = new Manager(m.name, m.id, m.email, m.officePhone);
+    managerArray.push(newManager);
+    console.log(managerArray);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getEngineer() {
+  try {
+    const e = await inquirer.prompt(questions.engineer);
+    const newEngineer = new Engineer(e.name, e.id, e.email, e.github);
+    engineerArray.push(newEngineer);
+    console.log(engineerArray);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getIntern() {
+  try {
+    const i = await inquirer.prompt(questions.intern);
+    const newIntern = new Intern(i.name, i.id, i.email, i.school);
+    internArray.push(newIntern);
+    console.log(internArray);
   } catch (error) {
     console.log(error);
   }
