4844ddcdd5bc1770d63d633ec49c9f72114dbf12
Tony Tran <codemonkey713@gmail.com>
2021-02-09 22:52:35 -0600

    completed setting up questions module for all scenario and type of user to add

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const questions = require('./lib/questions');
 // const open = require('open');
 // const employee = require('./lib/Employee');
 // const engineer = require('./lib/Engineer');
 // const intern = require('./lib/Intern');
 // const manager = require('./lib/Manager');
-const questions = require('./lib/questions');
-const Teamprofile = require('./lib/Teamprofile')
+// const questions = require('./lib/questions');
+// const Teamprofile = require('./lib/Teamprofile')
 
 async function start() {
   try {
-    const teamprofile = new Teamprofile();
-    teamprofile.askToStart();
+    await inquirer.prompt(questions.welcome);
+    
+    const manager = await inquirer.prompt(questions.manager);
+    console.log(manager);
+
+    
+
+    const engineer = await inquirer.prompt(questions.engineer);
+//     const teamprofile = new Teamprofile();
+//     teamprofile.askToStart();
 
     // const html = buildHTMLTemplate(userData);
     // fs.writeFileSync('./index.html', html);
-    console.log('File was successfully written.');
+    // console.log('File was successfully written.');
     // await open('./index.html');
-    console.log('Check your browser!');
+    // console.log('Check your browser!');
   } catch (error) {
     console.log(error);
   }
 }
 
 start();
+// const teamprofile = new Teamprofile();
+
+// teamprofile.askToStart();
